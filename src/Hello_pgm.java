0d0900db0d407ea1de7ac0392315a8f2fdfa9744
Shobhith Shobhi <shobhithm824@gmail.com>
2025-03-19 06:40:26 +0530

    init commit

--- src/Hello_pgm.java
+++ src/Hello_pgm.java
@@ -3,5 +3,6 @@ public class Hello_pgm {
     public static void main(String[] arg){
         //print hello word
         System.out.println("Hello World");
+        System.out.println("Hello World first program");
     }
 }
